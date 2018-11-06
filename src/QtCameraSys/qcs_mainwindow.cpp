12cc1f563cbd61c6e18e1e3f20facc578f112269
ErroMsg <gpy_1990@163.com>
2018-11-06 14:40:23 +0800

    begin coding

--- src/QtCameraSys/qcs_mainwindow.cpp
+++ src/QtCameraSys/qcs_mainwindow.cpp
@@ -13,17 +13,35 @@ QCS_MainWindow::QCS_MainWindow(QWidget *parent) :
     ui(new Ui::QCS_MainWindow)
 {
     ui->setupUi(this);
+
+//    VideoCapture video;
+//    Mat frame2;
+//    video.open("/home/gaopy/testData/test.avi");
+//    while (video.isOpened())
+//    {
+//        video >> frame2;
+//        imshow("video", frame2);
+//        if (cvWaitKey(40) == 27) //cvWaitKey的参数相当于多少ms一帧，现在是40ms一帧，1s25帧
+//            break; //按ESC就退出
+//    }
+
+/*
     Mat input_image;
     VideoCapture cam(0);
     if (!cam.isOpened())
         exit(0);
-    namedWindow("摄像头");
+
+    VideoWriter write;
+    write.open("/home/gaopy/testData/test.avi",VideoWriter::fourcc('M', 'J', 'P', 'G'), 30.0, Size(640, 480), true);
+
     while (true)
     {
         cam >> input_image;
-        imshow("input image", input_image);
-        if (27 == waitKey(30)) break;
-    }
+        imshow("VideoPlay", input_image);
+        write.write(input_image);
+        if (waitKey(30) == 27) break;
+
+    }*/
 
 }
 
